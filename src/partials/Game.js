11d28a00e1c0eb6d638672af95586e635bf2ba3e
Kerveland Richard <r.ikerveland@gmail.com>
2019-02-07 17:43:54 +0000

    Added Board class, added svg element via js, edited render methiod in Game class, added setting variable

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,38 @@
+import {SVG_NS } from '../settings';
+import Board from './Board';
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+    // Other code goes here...
+    this.gameElement= document.getElementById(this.element);
+    //Create a Board object and set width and height to game width and height
+    this.board = new Board(this.width, this.height);
+
   }
 
   render() {
-		// More code goes here....
+    // More code goes here....
+    //create an svg element
+
+    // Be sure to empty out before rendering
+    this.gameElement.innerHTML = '';
+
+    let svg = document.createElementNS(SVG_NS, 'svg');
+
+    //Set attributes
+    svg.setAttributeNS(null, 'width', this.width);
+    svg.setAttributeNS(null, 'height', this.height);
+    svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+
+    // append svg to our game element (selected by id)
+    this.gameElement.appendChild(svg);
+
+    // render the game components inside the SVG
+    this.board.render(svg);
+
   }
 }
